e2f908019ca7db0ebe6a647a2c18b07410234f2d
Charitha <151384903+charitha-26@users.noreply.github.com>
2026-04-22 21:35:55 +0530

    Add new CORS origin for production frontend

--- HirePath/hirepath-backend/src/main/java/com/hirepath/controller/BatchTrainingController.java
+++ HirePath/hirepath-backend/src/main/java/com/hirepath/controller/BatchTrainingController.java
@@ -19,7 +19,12 @@ import com.hirepath.service.BatchTrainingService;
 
 @RestController
 @RequestMapping("/api/batch-training")
-@CrossOrigin(origins = {"http://localhost:8081", "http://localhost:5173", "http://127.0.0.1:5173"})
+@CrossOrigin(origins = {
+    "http://localhost:5173",
+    "http://localhost:8081",
+    "http://127.0.0.1:5173",
+    "https://hirepath-frontend.onrender.com"  // ADD THIS LINE
+})
 public class BatchTrainingController {
 
     private final BatchTrainingAssignmentRepository batchTrainingAssignmentRepository;
