7de6672b69578efec3df78b647c0a1ed1b641473
Charitha <151384903+charitha-26@users.noreply.github.com>
2026-04-22 21:33:35 +0530

    Add CORS support for production frontend URL

--- HirePath/hirepath-backend/src/main/java/com/hirepath/controller/AdminController.java
+++ HirePath/hirepath-backend/src/main/java/com/hirepath/controller/AdminController.java
@@ -26,7 +26,12 @@ import com.hirepath.repository.UserRepository;
 
 @RestController
 @RequestMapping("/api/admin")
-@CrossOrigin(origins = {"http://localhost:8081", "http://localhost:5173", "http://127.0.0.1:5173"})
+@CrossOrigin(origins = {
+    "http://localhost:5173",
+    "http://localhost:8081",
+    "http://127.0.0.1:5173",
+    "https://hirepath-frontend.onrender.com"  // ADD THIS LINE
+})
 public class AdminController {
 
 	@Autowired
