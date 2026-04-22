a03bb7ea5c29fc0f31f952f99e08c9126a38ae6e
Charitha <151384903+charitha-26@users.noreply.github.com>
2026-04-22 21:38:53 +0530

    Add new origin to CrossOrigin annotation

--- HirePath/hirepath-backend/src/main/java/com/hirepath/controller/StudentTrainingController.java
+++ HirePath/hirepath-backend/src/main/java/com/hirepath/controller/StudentTrainingController.java
@@ -18,7 +18,12 @@ import com.hirepath.service.StudentTrainingService;
 
 @RestController
 @RequestMapping("/api/student-training")
-@CrossOrigin(origins = {"http://localhost:8081", "http://localhost:5173", "http://127.0.0.1:5173"})
+@CrossOrigin(origins = {
+    "http://localhost:5173",
+    "http://localhost:8081",
+    "http://127.0.0.1:5173",
+    "https://hirepath-frontend.onrender.com"  // ADD THIS LINE
+})
 public class StudentTrainingController {
 
     private final StudentTrainingRepository studentTrainingRepository;
